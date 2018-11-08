27236a37d602a659133c6667b87af62d634ee469
datquocngo <shaco14091999@gmail.com>
2018-11-08 08:19:07 +0700

    add

---
+++ fizz-buzz/src/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        System.out.println("15 is "+ FizzBuzz.fizzBuzz(15));
+        System.out.println("5 is "+ FizzBuzz.fizzBuzz(5));
+        System.out.println("9 is "+ FizzBuzz.fizzBuzz(9));
+        System.out.println("11 is "+ FizzBuzz.fizzBuzz(11));
+    }
+}
